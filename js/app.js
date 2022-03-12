22e308a06e013311e42e53293a4ce7afe023df91
rasedul99 <rasedul2199@gmail.com>
2022-03-13 01:21:34 +0600

    replace innerHtml to innerText

--- js/app.js
+++ js/app.js
@@ -154,7 +154,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
-  document.getElementById("liked").innerHTML = "";
+  document.getElementById("liked").innerText = "";
   likedPosts.forEach((post) => {
     const div = createPost(post);
   });
@@ -162,7 +162,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  document.getElementById("reported").innerHTML = "";
+  document.getElementById("reported").innerText = "";
   console.log(reportedPosts);
   reportedPosts.forEach((post) => {
     console.log(post);
