3b0f17feb3c3f8576e4782c5cb083a13e590d5e2
rasedul99 <rasedul2199@gmail.com>
2022-03-12 15:36:25 +0600

    text below 30 shown

--- js/app.js
+++ js/app.js
@@ -30,8 +30,7 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
   return text.length < 30
-    ? "text"
-    : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    ? `${text}`    : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
